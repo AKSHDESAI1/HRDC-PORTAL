82b1846c85394779a0abeabb7266461035905e29
AKSHDESAI1 <desaiaksh82@gmail.com>
2022-11-09 09:29:34 +0530

    protect route end-points

--- App/views.py
+++ App/views.py
@@ -11,7 +11,10 @@ from .models import Nomination
 
 
 def home(request):
-    return render(request, 'App/Home.html')
+    if request.user.is_authenticated:
+        return render(request, 'App/Home.html')
+    else:
+        return redirect("login")
 
 
 def login1(request):
@@ -45,62 +48,77 @@ def logout1(request):
 
 
 def announcement(request):
-    return render(request, 'App/announcement.html')
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return render(request, 'App/announcement.html')
+        else:
+            return HttpResponse("<h1> You do not have access to view this Page. Sorry!")
+    else:
+        return redirect("login")
 
 
 def list_announcement(request):
-    if request.user.is_superuser == True:
-        data = Event.objects.all()
-        context = {'data': data, 'super': True}
-        return render(request, 'App/list_announcement.html', context)
+    if request.user.is_authenticated:
+        if request.user.is_superuser == True:
+            data = Event.objects.all()
+            context = {'data': data, 'super': True}
+            return render(request, 'App/list_announcement.html', context)
+        else:
+            data = Event.objects.all()
+            context = {'data': data, 'super': False}
+            return render(request, 'App/list_announcement.html', context)
     else:
-        data = Event.objects.all()
-        context = {'data': data, 'super': False}
-        return render(request, 'App/list_announcement.html', context)
+        return redirect("login")
 
 
 def add_announcement(request):
-    if request.method == 'POST':
-        print("rudra", request.POST)
-        academicyear = request.POST['academicyear']
-        eventname = request.POST['eventname']
-        eventfocus = request.POST['eventfocus']
-        eventstartdate = request.POST['eventstartdate']
-        eventenddate = request.POST['eventenddate']
-        eventvenue = request.POST['eventvenue']
-        eventresourceperson = request.POST['eventresourceperson']
-        starttime = []
-        endtime = []
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            if request.method == 'POST':
+                print("rudra", request.POST)
+                academicyear = request.POST['academicyear']
+                eventname = request.POST['eventname']
+                eventfocus = request.POST['eventfocus']
+                eventstartdate = request.POST['eventstartdate']
+                eventenddate = request.POST['eventenddate']
+                eventvenue = request.POST['eventvenue']
+                eventresourceperson = request.POST['eventresourceperson']
+                starttime = []
+                endtime = []
 
-        for i in range(1, (int(eventenddate[8:]) - int(eventstartdate[8:]) + 2)):
-            print(f'starttime{i}', request.POST[f'starttime{i}'])
-            print(f'endtime{i}', request.POST[f'endtime{i}'])
-            starttime.append(request.POST[f'starttime{i}'])
-            endtime.append(request.POST[f'endtime{i}'])
+                for i in range(1, (int(eventenddate[8:]) - int(eventstartdate[8:]) + 2)):
+                    print(f'starttime{i}', request.POST[f'starttime{i}'])
+                    print(f'endtime{i}', request.POST[f'endtime{i}'])
+                    starttime.append(request.POST[f'starttime{i}'])
+                    endtime.append(request.POST[f'endtime{i}'])
 
-        print("starttime", starttime)
-        print("endtime", endtime)
+                print("starttime", starttime)
+                print("endtime", endtime)
 
-        doc = Event(academicyear=academicyear, eventname=eventname, eventfocus=eventfocus, eventstartdate=eventstartdate,
-                    eventenddate=eventenddate, eventvenue=eventvenue, eventresourceperson=eventresourceperson, starttime=starttime, endtime=endtime)
-        doc.save()
+                doc = Event(academicyear=academicyear, eventname=eventname, eventfocus=eventfocus, eventstartdate=eventstartdate,
+                            eventenddate=eventenddate, eventvenue=eventvenue, eventresourceperson=eventresourceperson, starttime=starttime, endtime=endtime)
+                doc.save()
 
-        # print(starttime)
-        # print(endtime)
-        # print(eventstartdate[8:])
-        # print(eventenddate[8:])
-        # print(int(eventenddate[8:]) - int(eventstartdate[8:]) + 1)
-        # print('starttime1', request.POST['starttime1 '])
+                # print(starttime)
+                # print(endtime)
+                # print(eventstartdate[8:])
+                # print(eventenddate[8:])
+                # print(int(eventenddate[8:]) - int(eventstartdate[8:]) + 1)
+                # print('starttime1', request.POST['starttime1 '])
 
-        # print(f"academmicyear {academicyear}")
-        # print(f"eventname {eventname}")
-        # print(f"eventfocus {eventfocus}")
-        # print(f"eventstartdate {eventstartdate}")
-        # print(f"eventenddate {eventenddate}")
-        # print(f"eventvenue {eventvenue}")
-        # print(f"eventresourceperson {eventresourceperson}")
-        return HttpResponseRedirect('/list_announcement')
-    return render(request, 'App/add_announcement.html')
+                # print(f"academmicyear {academicyear}")
+                # print(f"eventname {eventname}")
+                # print(f"eventfocus {eventfocus}")
+                # print(f"eventstartdate {eventstartdate}")
+                # print(f"eventenddate {eventenddate}")
+                # print(f"eventvenue {eventvenue}")
+                # print(f"eventresourceperson {eventresourceperson}")
+                return HttpResponseRedirect('/list_announcement')
+            return render(request, 'App/add_announcement.html')
+        else:
+            return HttpResponse("<h1> You do not have access to view this Page. Sorry!")
+    else:
+        return redirect("login")
 
 
 def edit_announcement(request, myid):
@@ -109,52 +127,80 @@ def edit_announcement(request, myid):
 
 
 def nomination(request):
-    if request.user.is_superuser:
-        a1 = Nomination.objects.all()
-        return render(request, 'App/admin_nomination.html', {'a1': a1})
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            a1 = Nomination.objects.all()
+            return render(request, 'App/admin_nomination.html', {'a1': a1})
+        else:
+            if request.method == "POST":
+                EmployeeName = request.POST['name']
+                EmployeeId = request.POST['id']
+                Branch = request.POST['branch']
+                FunctionDepartment = request.POST['fDepa']
+                ContactNo = request.POST['phone']
+                ContactNoMobile = request.POST['phone1']
+                ReportingAuthority = request.POST['authority']
+                TotalExperience = request.POST['experience']
+                TotalExperienceField = request.POST['expField']
+                TotalTeachingExperience = request.POST['teaExp']
+                TotalTeachingExperienceField = request.POST['teaExpfield']
+                Batch = request.POST['batch']
+
+                data = Nomination(EmployeeName=EmployeeName,
+                                EmployeeId=EmployeeId,
+                                Branch=Branch,
+                                FunctionDepartment=FunctionDepartment,
+                                ContactNo=ContactNo,
+                                ContactNoMobile=ContactNoMobile,
+                                ReportingAuthority=ReportingAuthority,
+                                TotalExperience=f'{TotalExperience} - {TotalExperienceField}',
+                                TotalTeachingExperience=f'{TotalTeachingExperience} - {TotalTeachingExperienceField}',
+                                Batch=Batch)
+                data.save()
+                messages.success(
+                    request, 'Nomination has been Succeffully Registered.')
+            return render(request, "App/nomination.html")
     else:
-        if request.method == "POST":
-            EmployeeName = request.POST['name']
-            EmployeeId = request.POST['id']
-            Branch = request.POST['branch']
-            FunctionDepartment = request.POST['fDepa']
-            ContactNo = request.POST['phone']
-            ContactNoMobile = request.POST['phone1']
-            ReportingAuthority = request.POST['authority']
-            TotalExperience = request.POST['experience']
-            TotalExperienceField = request.POST['expField']
-            TotalTeachingExperience = request.POST['teaExp']
-            TotalTeachingExperienceField = request.POST['teaExpfield']
-            Batch = request.POST['batch']
-
-            data = Nomination(EmployeeName=EmployeeName,
-                              EmployeeId=EmployeeId,
-                              Branch=Branch,
-                              FunctionDepartment=FunctionDepartment,
-                              ContactNo=ContactNo,
-                              ContactNoMobile=ContactNoMobile,
-                              ReportingAuthority=ReportingAuthority,
-                              TotalExperience=f'{TotalExperience} - {TotalExperienceField}',
-                              TotalTeachingExperience=f'{TotalTeachingExperience} - {TotalTeachingExperienceField}',
-                              Batch=Batch)
-            data.save()
-            messages.success(
-                request, 'Nomination has been Succeffully Registered.')
-        return render(request, "App/nomination.html")
-
+        return redirect("login")
 
 def nomination1(request):
-    return render(request, "App/1nomination.html")
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return render(request, "App/1nomination.html")
+        else:
+            return HttpResponse("<h1> You do not have access to view this Page. Sorry!")
+    else:
+        return redirect("login")
 
 
 def batchwise_nomination(request):
-    return render(request, "App/batchwise_nomination.html")
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return render(request, "App/batchwise_nomination.html")
+        else:
+            return HttpResponse("<h1> You do not have access to view this Page. Sorry!")
+    else:
+        return redirect("login")
+
 
 
 def download_nomi_admin(request):
-    return render(request, "App/download_nomi_admin.html")
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return render(request, "App/download_nomi_admin.html")
+        else:
+            return HttpResponse("<h1> You do not have access to view this Page. Sorry!")
+    else:
+        return redirect("login")
 
 
 def download_excel(request):
-    a1 = Nomination.objects.all()
-    return render(request, "App/admin_nomination.html", {'a1': a1})
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            a1 = Nomination.objects.all()
+            return render(request, "App/admin_nomination.html", {'a1': a1})
+        else:
+            return HttpResponse("<h1> You do not have access to view this Page. Sorry!")
+    else:
+        return redirect("login")
+
